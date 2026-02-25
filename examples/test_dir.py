a61f8f0107f943d17a3acee77d4f4d05e5a7ea33
Zeyu Xu <zeyu.xu@audiolabs-erlangen.de>
2026-02-25 15:31:27 +0100

    Possible future changes

--- examples/test_dir.py
+++ examples/test_dir.py
@@ -5,13 +5,18 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    
+
     # Use the Dir_Visualizer to create the balloon plot
     visualizer = Dir_Visualizer()
-    #visualizer.balloon_plot_with_slider()
+    # visualizer.balloon_plot_with_slider()
 
     # Comparison & Analysis for sofa files
     # Dir_Visualizer.experiment1()
     # Dir_Visualizer.experiment2()
     # Dir_Visualizer.experiment3()
-    #plt.show()
+    Dir_Visualizer.experiments(
+        exp="if_reciprocity",
+        filename="P0001_FreeFieldComp_48kHz.sofa",
+        if_fill_missing_dirs=True,
+    )
+    plt.show()
